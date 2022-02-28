abc0bab85d7a86f2702d2036813a8e8d2de58437
Richard <richardwang1337@gmail.com>
2022-02-28 16:19:04 -0600

    Create group/party for backend

--- java/src/main/java/com/techelevator/controller/RestaurantTinderController.java
+++ java/src/main/java/com/techelevator/controller/RestaurantTinderController.java
@@ -1,11 +1,41 @@
 package com.techelevator.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.techelevator.dao.PartyDao;
+import com.techelevator.dao.UserDao;
+import com.techelevator.dao.RestaurantDao;
+import com.techelevator.model.*;
+import com.techelevator.security.jwt.TokenProvider;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Part;
+import javax.validation.Valid;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/")
 public class RestaurantTinderController {
+
+    PartyDao partyDao;
+    RestaurantDao restaurantDao;
+
+    public RestaurantTinderController(PartyDao partyDao, RestaurantDao restaurantDao) {
+        this.partyDao = partyDao;
+        this.restaurantDao = restaurantDao;
+}
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(value = "/create_group", method = RequestMethod.POST)
+    public void createGroup(@Valid @RequestBody PartyDTO partyDTO) {
+        Party party = new Party();
+        party.setName(partyDTO.getEventName());
+        party.setHostId(partyDTO.getHostId());
+        party.setEndDate(partyDTO.getEndDate());
+        party.setHasEnded(partyDTO.isHasEnded());
+
+        partyDao.create(party);
+    }
+
 }
--- java/src/main/java/com/techelevator/model/PartyDTO.java
+++ java/src/main/java/com/techelevator/model/PartyDTO.java
@@ -17,4 +17,43 @@ public class PartyDTO {
     @NotEmpty
     private boolean hasEnded;
 
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public int getHostId() {
+        return hostId;
+    }
+
+    public void setHostId(int hostId) {
+        this.hostId = hostId;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public Timestamp getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Timestamp endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isHasEnded() {
+        return hasEnded;
+    }
+
+    public void setHasEnded(boolean hasEnded) {
+        this.hasEnded = hasEnded;
+    }
 }
