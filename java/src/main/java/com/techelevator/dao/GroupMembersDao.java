7e9a005c4631b25a5e65774d0b4d262d3c0049a8
Deatrice Clark <DeatriceClark@gmail.com>
2022-03-06 13:44:34 -0500

    moved to controller folder

--- java/src/main/java/com/techelevator/dao/GroupMembersDao.java
+++ java/src/main/java/com/techelevator/dao/GroupMembersDao.java
@@ -1,12 +1,24 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.GroupMembers;
+import com.techelevator.model.Restaurant;
+
+import java.util.List;
+
 public interface GroupMembersDao {
 
-
     //Find all members of a certain group by group id
+    List<GroupMembers> findByGroupId(int groupId);
 
+    //function to vote using user_vote, if else add vote to group restaurant list
+    // true false if true add to vote if not -1
 
-    //create the url
+    public GroupMembers createVote(int member_id, String member_name, String member_url, int group_id, int user_vote);
+
+    //get list of restaurants for the group so each user can vote on
+    //restaurant
+
+    public List<Restaurant> sendListToGroupByGroupId(int groupId);
 
 
 }
--- java/src/main/java/com/techelevator/dao/JdbcGroupMembersDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcGroupMembersDao.java
@@ -1,6 +1,7 @@
 package com.techelevator.dao;
 
 import com.techelevator.model.GroupMembers;
+import com.techelevator.model.Restaurant;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
@@ -18,17 +19,6 @@ public class JdbcGroupMembersDao implements GroupMembersDao{
         }
 
 
-//        public List<Restaurant> findRestaurant(String location) {
-//            List<Restaurant> restaurant = new ArrayList<>();
-//            String sql = "SELECT * FROM restaurants WHERE city LIKE ? OR zip_code LIKE ?";
-//            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, location, location);
-//            while(results.next()) {
-//                restaurant.add(mapRowToRestaurant(results));
-//            }
-//            return restaurant;
-//
-//        }
-
         private GroupMembers mapRowToGroupMembers(SqlRowSet rs) {
             GroupMembers groupMembers = new GroupMembers();
             groupMembers.setMember_id(rs.getInt("member_id"));
@@ -39,6 +29,19 @@ public class JdbcGroupMembersDao implements GroupMembersDao{
             return groupMembers;
         }
 
+    @Override
+    public List<GroupMembers> findByGroupId(int groupId) {
+        return null;
     }
 
-    //}
+    @Override
+    public GroupMembers createVote(int member_id, String member_name, String member_url, int group_id, int user_vote) {
+        return null;
+    }
+
+    @Override
+    public List<Restaurant> sendListToGroupByGroupId(int groupId) {
+        return null;
+    }
+}
+
