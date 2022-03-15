2cba72180c5c9a34d52f1e91d772f051553d64f4
Richard <richardwang1337@gmail.com>
2022-03-14 19:46:28 -0500

    changed name to groups

---
+++ frontend/src/Components/Group/Group.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Component, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { addToken, addUser } from '../../Redux/actionCreators'
+import { baseUrl } from '../../Shared/baseUrl'
+import axios from 'axios'
+import '../../style.css'
+import Card from '../UI/Card'
+
+class Group extends Component{
+
+    constructor(props){
+        super(props);
+    }
+
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+
+}
+
+export default Group;
